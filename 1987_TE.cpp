c1cbd831784c74740cc795e4e6c913a94b42d666
fjvbn2003 <fjvbn2003@gmail.com>
2020-03-01 12:23:59 +0000

    1987 풀었다 ! 내일 블로그 포스팅 각!

--- 1987_TE.cpp
+++ 1987_TE.cpp
@@ -4,30 +4,29 @@
 using namespace std;
 int r, c;
 char arr[20][20];
+int memo[25]= {0,};
 int ans = 0;
 //상하좌우
 int dy[] = {1,-1,0,0};
 int dx[] = {0, 0,-1,1};
-void dfs(int y, int x, string cc){
+void dfs(int y, int x, int l){
 	//선형탐색으로 지금까지 방문했던 문자를 현재위치의 문자와 일치하는지 검사
-	for(int i=0; i<cc.size(); i++){
-		if(arr[y][x] == cc[i]){
-			//일치한다면 바로 함수 종료
-			return;
-		}
-	}
-	// c에 현재의 문자를 추가
-	cc += arr[y][x];
-	if(ans < cc.size()){
-		ans = cc.size();
+	// 선형탐색을 hash로 검사해보자.!! 존재하는지 검사!!
+	// 1000000 * 1 = 백만!
+	
+	if(memo[arr[y][x]-65] == 1){return;}
+	memo[arr[y][x]-65] = 1;
+	if(l>ans){
+		ans = l;
 	}
 	for(int i=0; i<4; i++){
 		int ny = y+dy[i];
 		int nx = x+dx[i];
 		if(ny>=0 && ny <r && nx>=0 && nx <c){
-			dfs(ny,nx,cc);
+			dfs(ny,nx,l+1);
 		}
 	}
+	memo[arr[y][x]-65] = 0;
 	
 }
 
@@ -41,7 +40,7 @@ int main(){
 			arr[i][j] = t[j];
 		}
 	}
-	dfs(0,0,"");
+	dfs(0,0,1);
 	// for(int i=0; i<r; i++){
 	// 	for(int j=0; j<c; j++){
 	// 		cout <<arr[i][j]<< " ";
